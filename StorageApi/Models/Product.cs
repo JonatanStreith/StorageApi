b828af6a838b56671633e5dcefcce95b19042f1a
Jonatan Streith <jonatan@streith.se>
2026-06-09 16:41:17 +0200

    Minor improvements

--- StorageApi/Models/Product.cs
+++ StorageApi/Models/Product.cs
@@ -1,8 +1,16 @@
-﻿namespace StorageApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StorageApi.Models
 {
     public class Product
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string? Name { get; set; }
         public int Price { get; set; }
         public string? Category { get; set; }
